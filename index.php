967e060b1eb7a8189db7b9d5965919f3f8bbb3f9
andikacikke <50942384+andikacikke@users.noreply.github.com>
2019-10-11 23:42:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "<center>
+<h3>AWAN-4</h3>
+<table width = 300 border =1>
+<tr>
+<td>Nama</td>
+<td>Andika Dirgantara</td>
+</tr>
+<tr>
+<td>NIM</td>
+<td>10116291</td>
+</tr>
+</table>";
+?>
